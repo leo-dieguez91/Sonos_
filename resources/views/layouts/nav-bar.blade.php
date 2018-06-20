0ddfa71a82eae8e1a8b487a4e9f11745c9ea4513
leo-dieguez91 <leo.dieguez@gmail.com>
2018-06-20 16:38:25 -0300

    master

---
+++ resources/views/layouts/nav-bar.blade.php
@@ -0,0 +1,110 @@
+<header class="header-main fixed-top"> <!-- fixed-top -->
+  <div class="header-sup ">
+
+    <div class="row justify-content-between">
+      <div class="align-items-start col-1 col-lg-6">
+        <div class="col-6">
+          <nav class="navbar navbar-expand-lg p-0">
+            <div class="collapse navbar-collapse">
+
+              <a href="/"><img src="image/logosm.png" alt="logosm" >  </a>
+
+              <ul class="navbar-nav mt-2">
+                <li class="nav-item active">
+                  <a class="nav-link decoracion-borde" href="/">Home</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link decoracion-borde" href="/faq">Acerca de Sonos!</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link decoracion-borde" href="/#QuienesSomos">Quienes Somos</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Calendario</a>
+                </li>
+              </ul>
+
+            </div>
+          </nav>
+
+        </div>
+
+
+        <div class="nav-mobie">
+          <div id="mySidenav" class="sidenav">
+            <a href="index.php"><img src="image/logosm.png" alt="logosm"></a>
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a class="nav-link" href="bienvenido.php">Home</a>
+            <a href="/faq">Acerca de Sonos!</a>
+            <a href="/#QuienesSomos">Quienes Somos</a>
+            <a href="#">Grupos</a>
+            <a href="#">Calendario</a>
+            <a href="#">Ciudades</a>
+          </div>
+          <span class="ion-navicon-round"style="font-size:1.8em;cursor:pointer;line-height:55px;color:black " onclick="openNav()"></span>
+        </div>
+      </div>
+
+        <div class="col-10 col-lg-5 p-1">
+          <form class="col-lg-12" method="post">
+            <div class="form-inline ">
+              <div class="col-10 col-sm-9 col-md-8 col-lg-8 p-0 ml-lg-auto">
+                <div class="input-group mr-2 ">
+
+                    <input type="text" name="email" class="form-control mr-2 col-7" id="inlineFormInputName2" placeholder="Email" value="" data-toggle="tooltip" title="" value="{{old('email')}}">
+                    <!-- <span class="errores2">?></span> -->
+
+                    <input type="password" name="pass" class="form-control col-5" id="inlineFormInputName2" placeholder="Contraseña" value="" data-toggle="tooltip" title="">
+                    <!-- <span class="errores2">?></span> -->
+
+                </div>
+              </div>
+              <button type="submit" class="btn btn-sm ml-1 pl-2 pr-2 hidden-xs" style="font-size: 0.6em;font-weight:bold;" name="login"> <span class="ion-log-in"></span> Ingresar </button>
+              <button type="submit" class="btn btn-sm button-small ml-2 p-0 px-2"style="font-size: 0.7em;font-weight:bold;" name="login"> <span class="ion-log-in"></span></button>
+            </div>
+            <div class="form-check ajusteleft">
+              <label class="form-check-label" style="font-size: 0.7em; color:black;"><input class="form-check-input" type="checkbox" name="recordar">Recordarme</label>
+              <a href="#" class="ml-1 ml-sm-5">¿Olvidaste tu contraseña?</a>
+            </div>
+          </form>
+        </div>
+    </div>
+  </div>
+</header>
+
+<!-- boton top -->
+<button onclick="topFunction()" id="top-button" title="Go to top" class="fa fa-arrow-up"></button>
+
+
+<script>
+// cuando scrolea para abajo aparece el boton
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("top-button").style.display = "block";
+  } else {
+    document.getElementById("top-button").style.display = "none";
+  }
+}
+
+// cuando se hace click al boton sube para arriba
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "270px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+$('[data-toggle="tooltip"]').tooltip('show');
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.body')
+
+@section('section')
+  <!-- Registro & portada -->
+  <div class="portada-inicial justify-content-center">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 mt-3">
+          <img src="image/logo.png" alt="logo" class="mt-5" style="width: 100%; max-width: 405px;">
+          <em class="titulos d-none d-sm-block text-dark mb-5">La música nos une</em>
+          <br><br>
+          <div class="row justify-content-center">
+            <div class="col-8 col-md-6 col-lg-6 row-registro mb-4">
+              <form class="mt-3" method="post" enctype="multipart/form-data" action="/register">
+                @csrf
+
+                <div class="col-lg-5 d-inline-block float-left p-0">
+                  <input class="form-control" type="text" placeholder="Nombre" name="name" value="{{ old('name') }}">
+                  <label class="errores">
+
+                      @foreach ($errors->get('name') as $error)
+                        {{ $error }}
+                      @endforeach
+
+                  </label>
+                </div>
+
+                <div class="col-lg-6 ml-sm-auto d-inline-block float-right p-0">
+                  <input class="form-control" type="text" placeholder="Apellido" name="last_name" value="{{ old('last_name') }}">
+                  <label class="errores">
+
+                      @foreach ($errors->get('last_name') as $error)
+                        {{ $error }}
+                      @endforeach
+
+                  </label>
+                </div>
+
+
+                <input class="form-control" type="text" placeholder="Ingresá tu e-mail" name="email" value="{{ old('email') }}">
+                <label for="email" class="errores">
+                  @foreach ($errors->get('email') as $error)
+                      {{ $error }}
+                  @endforeach
+                </label>
+
+                <input class="form-control" type="password" placeholder="Creá tu contraseña" name="password">
+                <label class="errores">
+                  @foreach ($errors->get('password') as $error)
+                      {{ $error }}
+                  @endforeach
+                </label>
+
+                <div class="form-group">
+                  <button class="btn my-2" type="submit" name="register">Registrarme</button>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('section2')
+  <!--  Quienes Somos -->
+  <section class="container-fluid border fondoNeutro justify-content-center align-items-center" id="QuienesSomos">
+    <h1 class="my-5 text-center display-3">Quienes somos</h1>
+    <div class="col text-center border py-3 mb-5">
+      <p>En <em>Sonos!</em> buscamos reunir personas con la misma pasión por la música. <br>
+        Queremos que te encuentres con tus amigos, que hagas nuevos amigos y que puedas organizar tu próximo encuentro. <br>
+        Qué mejor que disfrutar del próximo recital de tu banda favorita con ellos! <br> <br>
+        Para hacerlo posible, nosotros te damos las herramientas: <br>
+        Podrás consultar nuestro Calendario con la información de los próximos eventos en tu ciudad. <br>
+        Podrás seguir a artistas y a otros usuarios. <br>
+        Recibirás notificaciones por email de los shows programados y alertas cuando salen las entradas a la venta, así no te perdés de nada. <br><br>
+        Lo mejor de esto, es que es 100% gratis. <br><br>
+        Que esperas! <strong><a href="#">Registrate</a></strong> y deciles a tus amig@s!<br><br>
+        La música nos une.
+        Somos, <em>Sonos!</em>
+      </p>
+      <label>
+        <button type="" class="btn my-2" name="button"><a href="/faq" >¿Aún tenés dudas?</a></button>
+      </label>
+    </div>
+  </section>
+
+@endsection
