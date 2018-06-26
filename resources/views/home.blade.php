6a2e6b849d622e7f08301f2c67bee33d5bd8ffa6
leo-dieguez91 <leo.dieguez@gmail.com>
2018-06-25 22:41:39 -0300

    modifique grilla del home menos codigo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
   <!-- Timeline content -->
   <br><br><br>
   <div class="container">
-    <div class="row align-items-center">
-      <div class="col-12 mt-3">
-        <div class="row justify-content-center">
+    <div class="row col-12 mt-3 d-block">
+
+
 
           {{-- Formulario de posteo --}}
-          <div class="col-8 col-md-6 col-lg-6 row-registro mb-4">
+          <div class="col-8 col-md-6 row-registro mb-4 mx-auto">
             <form class="mt-3" method="post" enctype="multipart/form-data" action="{{ route('register') }}">
               @csrf
               <textarea class="form-control" name="message" rows="2" cols="80" data-info='mensaje' placeholder="En que estas pensando?"></textarea>
@@ -20,50 +20,43 @@
           </div>
 
           {{-- Panel de Posteos --}}
-          <div class="container">
-            <div class="row align-items-center">
-              <div class="col-12 mt-3">
-                <div class="row justify-content-center">
-                  <div class="col-8 col-md-6 col-lg-6 row-registro mb-4">
-                    <h2>Posteos</h2>
-                    <p>Panel de posteos</p>
-                    <div class="card">
-                      <div class="card-header">
-                        <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
-                      </div>
-                      <div class="card-body">Kame Hame Ha!!</div>
-                      <div class="card-footer">
-                        <h6>Hace 5 segundos</h6>
-                      </div>
-                    </div>
-                    <br>
-                    <div class="card">
-                      <div class="card-header">
-                        <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
-                      </div>
-                      <div class="card-body">Kame Hame Ha!!</div>
-                      <div class="card-footer">
-                        <h6>Hace 5 segundos</h6>
-                      </div>
-                    </div>
-                    <br>
-                    <div class="card">
-                      <div class="card-header">
-                        <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
-                      </div>
-                      <div class="card-body">Kame Hame Ha!!</div>
-                      <div class="card-footer">
-                        <h6>Hace 5 segundos</h6>
-                      </div>
-                    </div>
-                    <br>
-                  </div>
-                </div>
+
+          <div class="col-8 col-md-6 row-registro mb-4 mx-auto">
+            <h2>Posteos</h2>
+            <p>Panel de posteos</p>
+            <div class="card">
+              <div class="card-header">
+                <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
+              </div>
+              <div class="card-body">Kame Hame Ha!!</div>
+              <div class="card-footer">
+                <h6>Hace 5 segundos</h6>
               </div>
             </div>
+            <br>
+            <div class="card">
+              <div class="card-header">
+                <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
+              </div>
+              <div class="card-body">Kame Hame Ha!!</div>
+              <div class="card-footer">
+                <h6>Hace 5 segundos</h6>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-header">
+                <h5>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} dice:</h5>
+              </div>
+              <div class="card-body">Kame Hame Ha!!</div>
+              <div class="card-footer">
+                <h6>Hace 5 segundos</h6>
+              </div>
+            </div>
+            <br>
           </div>
-        </div>
-      </div>
+
+
     </div>
   </div>
 @endsection
